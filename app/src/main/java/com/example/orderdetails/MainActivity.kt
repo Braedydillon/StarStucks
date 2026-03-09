c7a41d3d4e481d0fb820b440c716922284ec6038
Braedydillon <ST10439231@vcconnect.edu.za>
2026-03-09 15:49:45 +0200

    one

---
+++ app/src/main/java/com/example/orderdetails/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.orderdetails
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.orderdetails.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+binding.Coffee1.setOnClickListener {
+    Toast.makeText(this,"this is toast",Toast.LENGTH_SHORT)
+}
+
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+    }
+}
